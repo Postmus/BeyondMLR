de9709465725be097758fc013f810f707277f76c
Postmus <d.postmus@umcg.nl>
2024-11-27 19:36:26 +0100

    added assignment week 3

--- _backup.R
+++ _backup.R
@@ -1,89 +1,60 @@
-chicago_airbnb$overall_satisfaction <- sapply(
-  chicago_airbnb$overall_satisfaction,
-  function(x) {
-    rnorm(1, mean = x, sd = 1)  # Sample from N(mean = x, sd = 1)
-  }
-)
+# Define the column widths based on the positions given in the description
+column_widths <- c(5, 1, 4, 1, 3, 1, 3, 1, 3)
 
-n_neighborhoods <- length(unique(chicago_airbnb$neighborhood))  # Number of neighborhoods
-sigma_neighborhood <- 0.8 # Variability of the neighborhood effect
+# Read the SCI.DAT file using read.fwf()
+sci_data <- read.fwf("SCI.DAT", widths = column_widths, 
+                     col.names = c("Centre", "NA1", "CandidateID", "NA2", "Gender", "NA3", "WrittenScore", "NA4", "CourseworkScore"))
 
-# Generate random effects for neighborhoods
-neighborhood_effects <- rnorm(n_neighborhoods, mean = 0, sd = sigma_neighborhood)
+# Drop the NA columns that were added as placeholders for spaces
+sci_data <- sci_data[, c("Centre", "CandidateID", "Gender", "WrittenScore", "CourseworkScore")]
 
-# Add the neighborhood effect to the existing satisfaction ratings
-chicago_airbnb$overall_satisfaction <- chicago_airbnb$overall_satisfaction +
-  neighborhood_effects[as.numeric(chicago_airbnb$neighborhood)]
+# View the first few rows of the data
+head(sci_data)
 
-chicago_airbnb$overall_satisfaction <- chicago_airbnb$overall_satisfaction / (range(chicago_airbnb$overall_satisfaction)[2] - range(chicago_airbnb$overall_satisfaction)[1]) 
+# Write the data to a CSV file
+write.csv(sci_data, file = "sci_data.csv", row.names = FALSE)
 
-chicago_airbnb$overall_satisfaction <- 4*(chicago_airbnb$overall_satisfaction - min(chicago_airbnb$overall_satisfaction)) + 1
+###################
 
-##############################
+# Read the EMPLOY.DAT file using read.table()
+employ_data <- read.table("EMPLOY.DAT", 
+                          col.names = c("AreaID", "Cell", "Gender", "Qualification", "EmployedInCell", "TotalInCell"),
+                          fill = TRUE, strip.white = TRUE)
 
+# Convert numeric columns to appropriate data types
+employ_data$Gender <- as.numeric(employ_data$Gender)
+employ_data$Qualification <- as.numeric(employ_data$Qualification)
+employ_data$EmployedInCell <- as.numeric(employ_data$EmployedInCell)
+employ_data$TotalInCell <- as.numeric(employ_data$TotalInCell)
 
-summary(lmer(log_price ~ BikeScore + (1 | neighborhood), data = chicago_airbnb))
-summary(lmer(log_price ~ BikeScore + (1 | district/neighborhood), data = chicago_airbnb))
+# View the first few rows of the data
+head(employ_data)
 
-summary(lmer(log_price ~ bedrooms + (1 | neighborhood), data = chicago_airbnb))
-summary(lmer(log_price ~ bedrooms + (1 | district/neighborhood), data = chicago_airbnb))
+# Write the data to a CSV file
+write.csv(employ_data, file = "employ_data.csv", row.names = FALSE)
 
-summary(lmer(log_price ~ room_type + bedrooms + minstay + (1 | neighborhood), data = chicago_airbnb))
+#########################
 
-summary(lmer(log_price ~ TransitScore + (1 | neighborhood), data = chicago_airbnb))
-summary(lm(log_price ~ TransitScore, data = chicago_airbnb))
+# Define column widths based on the description provided for the ILEA567.DAT file
+dat_widths <- c(1, 3, 2, 2, 2, 1, 1, 2, 1, 1)
 
-summary(lmer(log_price ~ minstay + (1 | neighborhood), data = chicago_airbnb))
-summary(lm(log_price ~ minstay, data = chicago_airbnb))
+# Read the ILEA567.DAT file using read.fwf()
+ilea_data <- read.fwf("ILEA567.DAT", widths = dat_widths, 
+                      col.names = c("Year", "School", "ExamScore", "PercentFSM", "PercentVR1", "Gender", "VRBand", "EthnicGroup", "SchoolGender", "SchoolDenomination"),
+                      na.strings = c(" "), strip.white = TRUE)
 
-##########################
+# Convert relevant columns to factors
+ilea_data$Year <- factor(ilea_data$Year, levels = 1:3, labels = c("1985", "1986", "1987"))
+ilea_data$School <- ilea_data$School
+ilea_data$Gender <- factor(ilea_data$Gender, levels = 0:1, labels = c("Male", "Female"))
+ilea_data$VRBand <- factor(ilea_data$VRBand, levels = 1:3, labels = c("VR3", "VR1", "VR2"))
+ilea_data$EthnicGroup <- factor(ilea_data$EthnicGroup, levels = 1:11, labels = c("ESWI", "African", "Arab", "Bangladeshi", "Caribbean", "Greek", "Indian", "Pakistani", "S.E.Asian", "Turkish", "Other"))
+ilea_data$SchoolGender <- factor(ilea_data$SchoolGender, levels = 1:3, labels = c("Mixed", "Male", "Female"))
+ilea_data$SchoolDenomination <- factor(ilea_data$SchoolDenomination, levels = 1:3, labels = c("Maintained", "Church of England", "Roman Catholic"))
 
-summary(lmer(log_price ~ (overall_satisfaction_c + room_type + accommodates_c + 
-                                               bedrooms_c + minstay_c)*(TransitScore_c + BikeScore_c) 
-                                             + (1 | neighborhood), data = chicago_airbnb))
+# View the first few rows of the data
+head(ilea_data)
 
-step(lmer(log_price ~ (overall_satisfaction_c + room_type + accommodates_c + 
-                            bedrooms_c + minstay_c)*(TransitScore_c + BikeScore_c) 
-             + (1 | neighborhood), data = chicago_airbnb))
+# Write the data to a CSV file
+write.csv(ilea_data[, c(2:4, 6:7, 10)], file = "ilea_data.csv", row.names = FALSE)
 
-
-listing_summary <- chicago_airbnb %>%
-  group_by(district) %>%
-  summarise(num_neighborhoods = n_distinct(neighborhood))
-
-ggplot(listing_summary, aes(x = reorder(district, num_neighborhoods), y = num_neighborhoods)) +
-  geom_bar(stat = "identity", fill = "skyblue", color = "black") +
-  coord_flip() +
-  labs(title = "Number of Neighborhoods per District",
-       x = "Distruct",
-       y = "Number of Neighborhoods") 
-
-
-
-model.3level <- lmer(log_price ~ 1 + (1 | district/neighborhood), data = chicago_airbnb)
-summary(model.3level)
-
-model.2level <- lmer(log_price ~ 1 + (1 | neighborhood), data = chicago_airbnb)
-summary(model.2level)
-
-anova(model.2level, model.3level, refit = FALSE)
-
-model.3level.L1 <- lmer(log_price ~ overall_satisfaction_c + room_type + accommodates_c +
-                          bedrooms_c + minstay_c + (1 | district/neighborhood), data = chicago_airbnb)
-summary(model.3level.L1)
-
-model.2level.L1 <- lmer(log_price ~ overall_satisfaction_c + room_type + accommodates_c +
-                          bedrooms_c + minstay_c + (1 | neighborhood), data = chicago_airbnb)
-summary(model.2level.L1)
-
-anova(model.3level.L1)
-anova(model.2level.L1)
-
-model.3level.L2 <- lmer(log_price ~ TransitScore_c + BikeScore_c + (1 | district/neighborhood), data = chicago_airbnb)
-summary(model.3level.L2)
-
-model.2level.L2 <- lmer(log_price ~ TransitScore_c + BikeScore_c + (1 | neighborhood), data = chicago_airbnb)
-summary(model.3level.L2)
-
-anova(model.3level.L2)
-anova(model.2level.L2)
